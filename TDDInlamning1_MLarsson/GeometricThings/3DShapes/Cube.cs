87352413469eb52a9e95236b8985a1e512e22b27
Mikael Larsson <mikael_larsson84@hotmail.com>
2021-05-26 09:47:30 +0200

    Added cubeArea and folders in projects

---
+++ TDDInlamning1_MLarsson/GeometricThings/3DShapes/Cube.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDInlamning1_MLarsson.Tests;
+
+namespace TDDInlamning1_MLarsson.GeometricThings
+{
+    public class Cube : GeometricThing
+    {
+        public Cube(float side)
+        {
+            Side = side;
+        }
+
+        public float Side { get; set; }
+
+        public override float GetArea()
+        {
+            return MathF.Round( Area = Side < 0 ? 0 : GetOneSideArea() * 6, 2);
+        }
+
+        private float GetOneSideArea()
+        {
+            var square = new Square(Side);
+            return square.GetArea();
+        }
+
+        public override float GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+        public override float GetVolume()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
+
---
+++ TDDInlamning1_MLarssonTests/GeometricThingsTests/3DShapesTests/CubeTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDInlamning1_MLarsson.GeometricThings;
+
+namespace TDDInlamning1_MLarssonTests.GeometricThings
+{
+    [TestClass]
+    public class CubeTests
+    {
+        [TestMethod]
+        [DataRow(5f, 150f)]
+        public void GetArea_CubeTest_PositiveResult(float side, float expected)
+        {
+            var cube = new Cube(side);
+            var actual = cube.GetArea();
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        [DataRow(0f, 0f)]
+        [DataRow(-5f, 0f)]
+        [DataRow(null, 0f)]
+        public void GetArea_CubeTest_NegativeResult(float side, float expected)
+        {
+            var cube = new Cube(side);
+            var actual = cube.GetArea();
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
